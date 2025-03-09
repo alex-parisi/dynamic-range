d1df1cb539eebf3a147cffab56e13fc242c354bb
Alex Parisi <alex.parisi@outlook.com>
2025-03-09 00:06:53 -0500

    Added a limiter

---
+++ inc/Limiter.h
@@ -0,0 +1,170 @@
+/// Limiter.h
+
+/**
+Copyright © 2025 Alex Parisi
+
+Permission is hereby granted, free of charge, to any person obtaining a copy of
+this software and associated documentation files (the "Software"), to deal in
+the Software without restriction, including without limitation the rights to
+use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies
+of the Software, and to permit persons to whom the Software is furnished to do
+so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.
+*/
+
+#ifndef LIMITER_H
+#define LIMITER_H
+
+#include <optional>
+
+/**
+ * @brief Limiter configuration
+ * @tparam T Type of the limiter
+ */
+template<typename T = double>
+struct LimiterConfiguration {
+    int sampleRate = 0.0;
+    T threshold, attack, release;
+    std::optional<T> makeupGain = std::nullopt;
+    std::optional<T> kneeWidth = std::nullopt;
+};
+
+/**
+ * @brief Limiter class. Implements a limiter with optional makeup gain and
+ * knee width.
+ * @details The limiter is implemented as a class with a process method that
+ * takes a sample and processes it. The process method calculates the static
+ * characteristic of the limiter, updates the gain smoothing, and applies the
+ * makeup gain. The class also has a process method that takes an array of
+ * samples and processes them all.
+ * @tparam T Type of the limiter
+ */
+template<typename T = double>
+class Limiter {
+public:
+    /**
+     * @brief Public constructor
+     * @param configuration Limiter configuration
+     * @return A Limiter object
+     */
+    auto static create(LimiterConfiguration<T> configuration)
+            -> std::optional<Limiter> {
+        if (configuration.sampleRate <= 0) {
+            return std::nullopt;
+        }
+        if (configuration.attack <= 0 || configuration.release <= 0) {
+            return std::nullopt;
+        }
+        return Limiter(configuration);
+    }
+
+    /**
+     * @brief Process a sample in-place
+     * @param sample Sample to process
+     */
+    auto process(T &sample) -> void {
+        T inputDecibels = 20.0 * std::log10(sample);
+        T xSc = calculate_static_characteristic(inputDecibels);
+        update_gain_smoothing(xSc, inputDecibels);
+        T gM = m_gainSmoothing * m_config.makeupGain.value();
+        T gLin = std::pow(10.0, gM / 20.0);
+
+        sample *= gLin;
+    }
+
+    /**
+     * @brief Processes an array of samples in-place
+     * @param samples Pointer to the samples
+     * @param count Number of samples
+     */
+    auto process(T *samples, const size_t count) -> void {
+        for (int i = 0; i < count; i++) {
+            process(samples[i]);
+        }
+    }
+
+    /**
+     * @brief Resets the limiter
+     */
+    auto reset() -> void { m_gainSmoothing = 0.0; }
+
+private:
+    /**
+     * @brief Private constructor
+     * @param configuration Limiter configuration
+     */
+    explicit Limiter(LimiterConfiguration<T> configuration) :
+        m_config(configuration) {
+        /// If makeupGain is not specified, calculate it:
+        if (!m_config.makeupGain.has_value()) {
+            m_config.makeupGain = 0.0 - calculate_static_characteristic(0.0);
+        }
+    }
+
+    /**
+     * @brief Calculate the static characteristic of the limiter
+     * @param inputDecibels Input in decibels
+     * @return The static characteristic
+     */
+    T calculate_static_characteristic(T inputDecibels) {
+        if (m_config.kneeWidth.has_value()) {
+            /// Soft knee
+            if (inputDecibels <
+                (m_config.threshold - (m_config.kneeWidth.value() / 2.0))) {
+                return inputDecibels;
+            }
+            if (inputDecibels >
+                (m_config.threshold + (m_config.kneeWidth.value() / 2.0))) {
+                return m_config.threshold;
+            }
+            float numerator =
+                    std::pow(inputDecibels - m_config.threshold +
+                                     (m_config.kneeWidth.value() / 2.0),
+                             2.0);
+            return inputDecibels -
+                   (numerator /
+                    (2.0 * m_config.kneeWidth.value()));
+        }
+        /// Hard knee
+        if (inputDecibels < m_config.threshold) {
+            return inputDecibels;
+        }
+        return m_config.threshold;
+    }
+
+    /**
+     * @brief Update the gain smoothing
+     * @param xSc Static characteristic
+     * @param inputDecibels Input in decibels
+     */
+    void update_gain_smoothing(T xSc, T inputDecibels) {
+        float gC = xSc - inputDecibels;
+        float alpha = [this, &gC]() -> float {
+            if (gC <= m_gainSmoothing) {
+                return std::exp(-log10(9.0f) /
+                                (m_config.attack * m_config.sampleRate));
+            }
+            return std::exp(-log10(9.0f) /
+                            (m_config.release * m_config.sampleRate));
+        }();
+        m_gainSmoothing = alpha * m_gainSmoothing + (1.0f - alpha) * gC;
+    }
+
+    /** Limiter configuration */
+    LimiterConfiguration<T> m_config;
+
+    /** Gain smoothing */
+    T m_gainSmoothing = 0;
+};
+
+#endif // LIMITER_H
---
+++ test/LimiterTest.cpp
@@ -0,0 +1,95 @@
+/// LimiterTest.cpp
+
+#include <gtest/gtest.h>
+#include "Limiter.h"
+
+TEST(LimiterTest, CreateLimiterSuccess) {
+    constexpr auto config = LimiterConfiguration<float>{.sampleRate = 48000,
+                                                        .threshold = -10.0f,
+                                                        .attack = 0.01f,
+                                                        .release = 0.1f,
+                                                        .makeupGain = 5.0f,
+                                                        .kneeWidth = 5.0f};
+    const std::optional<Limiter<float>> limiter =
+            Limiter<float>::create(config);
+    ASSERT_TRUE(limiter.has_value());
+}
+
+TEST(LimiterTest, CreateLimiterFailureInvalidSampleRate) {
+    constexpr auto config = LimiterConfiguration<float>{.sampleRate = 0,
+                                                        .threshold = -10.0f,
+                                                        .attack = 0.01f,
+                                                        .release = 0.1f,
+                                                        .makeupGain = 5.0f,
+                                                        .kneeWidth = 5.0f};
+    const std::optional<Limiter<float>> limiter =
+            Limiter<float>::create(config);
+    ASSERT_FALSE(limiter.has_value());
+}
+
+TEST(LimiterTest, CreateLimiterFailureInvalidAttack) {
+    constexpr auto config = LimiterConfiguration<float>{.sampleRate = 48000,
+                                                        .threshold = -10.0f,
+                                                        .attack = 0.0f,
+                                                        .release = 0.1f,
+                                                        .makeupGain = 5.0f,
+                                                        .kneeWidth = 5.0f};
+    const std::optional<Limiter<float>> limiter =
+            Limiter<float>::create(config);
+    ASSERT_FALSE(limiter.has_value());
+}
+
+TEST(LimiterTest, CreateLimiterFailureInvalidRelease) {
+    constexpr auto config = LimiterConfiguration<float>{.sampleRate = 48000,
+                                                        .threshold = -10.0f,
+                                                        .attack = 0.0f,
+                                                        .release = -1.0f,
+                                                        .makeupGain = 5.0f,
+                                                        .kneeWidth = 5.0f};
+    const std::optional<Limiter<float>> limiter =
+            Limiter<float>::create(config);
+    ASSERT_FALSE(limiter.has_value());
+}
+
+TEST(LimiterTest, ProcessSampleBelowThreshold) {
+    constexpr auto config = LimiterConfiguration<float>{.sampleRate = 48000,
+                                                        .threshold = -10.0f,
+                                                        .attack = 0.01f,
+                                                        .release = 0.1f,
+                                                        .makeupGain = 5.0f,
+                                                        .kneeWidth = 5.0f};
+    std::optional<Limiter<float>> limiter = Limiter<float>::create(config);
+    ASSERT_TRUE(limiter.has_value());
+    float sample = 0.1f; // -20 dB
+    limiter->process(sample);
+    ASSERT_FLOAT_EQ(sample, 0.1f);
+}
+
+TEST(LimiterTest, ProcessSampleAboveThreshold) {
+    constexpr auto config = LimiterConfiguration<float>{.sampleRate = 48000,
+                                                        .threshold = -10.0f,
+                                                        .attack = 0.01f,
+                                                        .release = 0.1f,
+                                                        .makeupGain = 5.0f,
+                                                        .kneeWidth = 5.0f};
+    std::optional<Limiter<float>> limiter = Limiter<float>::create(config);
+    ASSERT_TRUE(limiter.has_value());
+    float sample = 1.0f; // 0 dB
+    limiter->process(sample);
+    ASSERT_LT(sample, 1.0f);
+}
+
+TEST(LimiterTest, ResetLimiter) {
+    constexpr auto config = LimiterConfiguration<float>{.sampleRate = 48000,
+                                                        .threshold = -10.0f,
+                                                        .attack = 0.01f,
+                                                        .release = 0.1f,
+                                                        .makeupGain = 5.0f,
+                                                        .kneeWidth = 5.0f};
+    std::optional<Limiter<float>> limiter = Limiter<float>::create(config);
+    ASSERT_TRUE(limiter.has_value());
+    limiter->reset();
+    float sample = 1.0f;
+    limiter->process(sample);
+    ASSERT_LT(sample, 1.0f);
+}
